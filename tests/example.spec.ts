641533d1e4c8f37dbe3c11909a42e68d77e200e5
Paraskevi Papa <papaparaskevi@yahoo.com>
2024-12-02 21:34:26 +0100

    Third commit webhook

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -9,7 +9,7 @@ test('has title', async ({ page }) => {
 
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-  console.log("The value is: " + process.env.PASSWORD)
+  //console.log("The value is: " + process.env.PASSWORD)
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
