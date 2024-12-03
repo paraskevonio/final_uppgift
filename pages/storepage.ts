89f464eb95551220fbfb7faac59eebfd11848d19
Paraskevi Papa <papaparaskevi@yahoo.com>
2024-12-03 19:37:55 +0100

    commit_12

--- pages/storepage.ts
+++ pages/storepage.ts
@@ -3,20 +3,18 @@ import { Locator, Page } from "@playwright/test";
 export class StorePage {
     readonly page: Page;
     readonly usernameText : Locator;
-    readonly header: Locator;
-    readonly usernameDisplay : Locator;
+    readonly header: Locator;    
     
     
     constructor(page: Page){
         this.page= page;
         this.usernameText = page.getByTestId('username') 
-        this.usernameDisplay = page.locator('#usernameDisplay');
         this.header = page.locator('h1')      
 
     }
     async getUsername(){
         this.page.waitForTimeout(200)
-        return this.usernameDisplay.textContent()
+        return this.usernameText.textContent()
     }
     
 }
